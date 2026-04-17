309e0711c5222f29af94ecbcb77b42283ce4085f
PiyushNMutha <muthapiyush@aca.edu.in>
2026-04-17 19:41:57 +0000

    testing

--- api/urls.py
+++ api/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', register),
     path('profile/', profile),
-    path('api/', include('cafeteria_tables.urls')),
+    # path('api/', include('cafeteria_tables.urls')),
     path('api/', include('menu.urls')),
-    path('api/', include('orders.urls')),
+    # path('api/', include('orders.urls')),
 ]
