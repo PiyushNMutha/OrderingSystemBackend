d47841739d8ed52b3b05ad46f5f0b3b63a313292
PiyushNMutha <piyushmutha04@gmail.com>
2026-05-10 11:36:36 +0530

    Dashboard-url-change

--- orders/urls.py
+++ orders/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('orders/', OrderListAPI.as_view(), name='order-list'),
     path('order/<int:order_id>/status/', UpdateOrderStatusAPI.as_view(), name='update-order-status'),
     path('order/<int:order_id>/track/', TrackOrderAPI.as_view(), name='track-order'),
-    path('dashboard/', AdminDashboardAPI.as_view(), name='admin-dashboard'),
+    path('admin/dashboard/', AdminDashboardAPI.as_view(), name='admin-dashboard'),
 ]
